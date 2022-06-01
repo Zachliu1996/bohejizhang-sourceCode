a4171963bdf981153a482d4b1e1251a2326e15cc
zachliu <zqliu643@outlook.com>
2022-06-01 23:08:47 +0800

    初步搭建路由

---
+++ src/views/Labels.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  labels
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Labels'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
